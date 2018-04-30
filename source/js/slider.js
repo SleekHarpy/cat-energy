873036f2baf47c3367c8956473666a438a463eaa
SleekHarpy <rtlistate@gmail.com>
2018-04-30 20:47:40 +0300

    Сделан слайдер примера а мобильной версии

---
+++ source/js/slider.js
@@ -0,0 +1,9 @@
+/*var divisor = document.querySelector(".comparison__divisor"),
+slider = document.querySelector(".comparison__slider");
+function moveDivisor() {
+	divisor.style.width = slider.value+"%";
+}*/
+
+function l_image (a) {
+ document.querySelector(".comparison__image").src=a
+}
